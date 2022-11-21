81bedf104c5acb89399f225d1d4ec9fcd5959cac
Jessicanguyenn81 <Jessicanguyenn81@gmail.com>
2022-11-21 10:23:20 -0800

    category and product model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const usersCtrl = require('../../controllers/api/users')
-const ensureLoggedIn = require('../../config/ensureLoggedIn')
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
-router.post('/', usersCtrl.create)
-router.post('/login', usersCtrl.login)
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
+// POST /api/users
+router.post('/', usersCtrl.create);
+// POST /api/users/login
+router.post('/login', usersCtrl.login);
 
-module.exports = router
+module.exports = router;
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,57 +1,62 @@
-// Rewrite the SignUpForm as a function component
-import { useState } from 'react'
-import { signUp } from '../../utilities/users-service'
+import { Component } from "react";
+import { signUp } from '../../utilities/users-service';
 
-export default function SignUpForm({ setUser }) {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        confirm: '',
-        error: ''
-    })
-    const disable = formData.password !== formData.confirm;
+export default class SignUpForm extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    confirm: '',
+    error: ''
+  };
 
-    function handleChange(evt) {
-        setFormData({
-            ...formData,
-            [evt.target.name]: evt.target.value,
-            error: ''
-        })
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+      error: ''
+    });
+  };
+
+  handleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const formData = {...this.state};
+      delete formData.confirm;
+      delete formData.error;
+      // The promise returned by the signUp service method
+      // will resolve to the user object included in the 
+      // payload of the JSON Web Token (JWT)
+      const user = await signUp(formData);
+      // Baby step 
+      this.props.setUser(user);
+    } catch {
+      // An error happened on the server
+      this.setState({ error: 'Sign Up Failed - Try Again' });
     }
+  };
 
-    async function handleSubmit(evt) {
-        evt.preventDefault()
-        try {
-            const formDataCopy = {...formData}
-            delete formDataCopy.error
-            delete formDataCopy.confirm
-            const user = await signUp(formDataCopy)
-            setUser(user)
-        } catch {
-            setFormData({
-                ...formData,
-                error: 'Sign Up Failed - Try Again'
-            })
-        }
-    }
-
-    return(
-        <div>
-            <div className="form-container">
-              <form autoComplete="off" onSubmit={handleSubmit}>
-                <label>Name</label>
-                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-                <label>Email</label>
-                <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-                <label>Password</label>
-                <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-                <label>Confirm</label>
-                <input type="password" name="confirm" value={formData.confirm} onChange={handleChange} required />
-                <button type="submit" disabled={disable}>SIGN UP</button>
-              </form>
-            </div>
-            <p className="error-message">&nbsp;{formData.error}</p>
-          </div>
-    )
+  // We must override the render method
+  // The render method is the equivalent to a function-based component
+  // (its job is to return the UI)
+  render() {
+    const disable = this.state.password !== this.state.confirm;
+    return (
+      <div>
+        <div className="form-container">
+          <form autoComplete="off" onSubmit={this.handleSubmit}>
+            <label>Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+            <label>Email</label>
+            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+            <label>Password</label>
+            <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+            <label>Confirm</label>
+            <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+            <button type="submit" disabled={disable}>SIGN UP</button>
+          </form>
+        </div>
+        <p className="error-message">&nbsp;{this.state.error}</p>
+      </div>
+    );
+  }
 }
